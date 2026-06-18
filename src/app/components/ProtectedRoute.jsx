3eccbad6ce511a5ed551900e5693b51c4ad234f8
Cristobal Vera <cristobalverar97@gmail.com>
2026-06-18 20:57:09 +0200

    added loading state handler to protected route

--- src/app/components/ProtectedRoute.jsx
+++ src/app/components/ProtectedRoute.jsx
@@ -2,10 +2,12 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 export function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
   
 
-  
+  if(loading){
+    return <p>Loading...</p>
+  }
   if (!isAuthenticated) {
     
     return <Navigate to="/" replace />;
